2e3a951eed4c75185fbc1b0065a652b6cc7c5aa2
Xnetter <ursingarrett@gmail.com>
2019-08-29 12:41:28 -0700

    Add Game Files

---
+++ Vampire Game/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform playerLoc;
+    public Vector3 displacement = new Vector3(0, 0, -10);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerLoc.position + displacement;
+    }
+}
---
+++ Vampire Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float jumpForce;
+    public float speed;
+    private float moveInput;
+    private float jumpInput;
+
+    private bool canJump;
+
+    private bool isGrounded;
+    private bool facingRight = true;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        jumpInput = Input.GetAxis("Vertical");
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+        if (isGrounded && Input.GetKeyDown(KeyCode.W))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
