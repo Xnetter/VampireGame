485e998baa6cf5a0c414e1c5fa7f6aa8763e1f4f
derekdude <dereksantolo@Dereks-MacBook-Pro.local>
2019-09-20 21:55:54 -0700

    Added Brackeys character controller scripts for 2D Movement.

--- Vampire Game/Assets/Scripts/PlayerMovement.cs
+++ Vampire Game/Assets/Scripts/PlayerMovement.cs
@@ -1,46 +1,41 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-	//public CharacterController2D controller; 
-	public float runSpeed = 40f; 
-	public float horizontalMove = 0f; 
-	bool jump = false; 
-	bool crouch = false; 
+public class PlayerMovement : MonoBehaviour {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public CharacterController2D controller;
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; 
+	public float runSpeed = 40f;
 
-        if (Input.GetButtonDown("Jump")) 
-        {
-        	jump = true; 
-        }
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+	
+	// Update is called once per frame
+	void Update () {
 
-        if (Input.GetButtonDown("Crouch")) 
-        {
-        	crouch = true; 
-        }
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        else if (Input.GetButtonUp("Crouch"))
-        {
-        	crouch = false; 
-        }
-    }
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
 
-    void FixedUpdate() 
-    {
-        Debug.Log("fixedUpdate"); 
-    	//controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump); 
-    }
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
+	}
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
 }
-     
